c03198e82262ccdfdf1acbd0e346d640d006ec9e
Simao Belchior <simaobelchior@gmail.com>
2021-10-27 23:52:02 +0100

    Start over

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-
-  resources :users
-  resources :sessions
-  root "home#index"
-end
