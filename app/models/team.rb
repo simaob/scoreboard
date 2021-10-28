827b5e43a951d69408fe5e1c4f78c510f95b2770
Simao Belchior <simaobelchior@gmail.com>
2021-10-28 19:52:41 +0100

    Add more display stuff

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,18 @@
 class Team < ApplicationRecord
   has_many :users
+  has_many :submissions, through: :users
+  has_many :votes, through: :users
+  has_many :votes_received, through: :submissions, source: :votes
+
+  def submissions_count
+    submissions.count
+  end
+
+  def votes_cast_count
+    votes.count
+  end
+
+  def votes_received_count
+    votes_received.count
+  end
 end
