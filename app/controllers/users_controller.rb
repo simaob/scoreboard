fb5bf94f58cff9a2acc986a0bf46e18c63e1bee0
Simao Belchior <simaobelchior@gmail.com>
2021-10-29 18:04:42 +0100

    Sort by name and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.order(:name).page(params[:page])
+    @users = User.order(:name, :email).page(params[:page])
   end
 
   # GET /users/1
